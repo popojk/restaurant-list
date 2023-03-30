3090b14fce74ecfd5609cc3a2f116ba6be892cc2
vinchiengithub <jianfan0114@gmail.com>
2023-03-30 17:59:27 +0800

    setting template engine

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// require express-handlebars
+const exphbs = require('express-handlebars');
+
+const restaurantList = require('./restaurant.json');
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// setting static files
+app.use(express.static('public'));
+
 // routes setting
 app.get('/', (req, res) => {
-  res.send('This is my restaurant list built with Express');
+  res.render('index', { restaurants: restaurantList.results });
 });
 
 // start and listen on the Express server
