c2797f753331eab072251a17bf2fe895239d7dfa
vinchiengithub <jianfan0114@gmail.com>
2023-03-30 23:05:58 +0800

    create restaurant seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
+
 const restaurantSchema = new Schema({
   name: { type: String, require: true },
   name_en: { type: String, require: true },
@@ -11,4 +13,5 @@ const restaurantSchema = new Schema({
   rating: { type: Number, require: true },
   description: { type: String, require: true },
 });
+
 module.exports = mongoose.model('restaurant', restaurantSchema);
---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+// require restaurant model
+const restaurant = require('../restaurant');
+const restaurantList = require('../../restaurant.json').results;
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', () => {
+  console.log('mongodb error!');
+});
+
+db.once('open', () => {
+  console.log('running restaurantSeeder.js ...');
+
+  restaurant
+    .create(restaurantList)
+    .then(() => {
+      console.log('restaurantSeeder done!');
+      db.close();
+    })
+    .catch((err) => console.log(err));
+});
