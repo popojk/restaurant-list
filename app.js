26101ef241e6394736431c250dde6384c740f358
vinchiengithub <jianfan0114@gmail.com>
2023-03-31 15:28:55 +0800

    create update function

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ const bodyParser = require('body-parser');
 // 每筆請求都要透過 body-parser 作前置處理
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// require methodOverride
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 // get database connect status
 const db = mongoose.connection;
 // connect error
@@ -53,6 +57,29 @@ app.get('/', (req, res) => {
     .lean()
     .then((restaurants) => res.render('index', { restaurants: restaurants }))
     .catch((error) => console.error(error));
+});
+
+// edit
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id;
+  restaurant
+    .findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant: restaurant }))
+    .catch((error) => console.log(error));
+});
+
+app.put('/restaurants/:id', (req, res) => {
+  const id = req.params.id;
+  const updateData = req.body;
+  restaurant
+    .findByIdAndUpdate(id, updateData)
+    .then(() => {
+      res.redirect(`/restaurants/${id}`);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 // new
