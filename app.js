ce41916f10597c93c85881dede94c861a67166ac
popojk <c.alexwu@gmail.com>
2023-04-09 13:05:30 +0800

    feat: initialize 2nd version that includes login function

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // require packages used in the project
 const express = require('express');
 const app = express();
-const port = 3000;
 // require express-handlebars
 const exphbs = require('express-handlebars');
 // require body-parser
@@ -13,7 +12,11 @@ const routes = require('./routes');
 
 // require mongoose config
 require('./config/mongoose');
-
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const port = process.env.PORT;
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
