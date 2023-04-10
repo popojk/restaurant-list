3e7b2e9bf7e5cf1b673bba98d98e9653cfe03bc0
popojk <c.alexwu@gmail.com>
2023-04-11 00:18:11 +0800

    feat: add facebook login function

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,14 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const user = require('./modules/user')
+const auth = require('./modules/auth')
 const {authenticator} = require('../middleware/auth')
 
 
 router.use('/restaurants', authenticator, restaurants)
 router.use('/search', authenticator, search)
 router.use('/user', user)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 // export router
