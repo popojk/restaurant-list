ea0bf5862533e2e7c90f8ae269d0710ce69237c2
popojk <c.alexwu@gmail.com>
2023-04-11 20:09:54 +0800

    feat: fix search route to only query restaurant of the login user

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 // require packages used in the project
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 // require express session
 const session = require('express-session')
 // require express-handlebars
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars')
 // require body-parser
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 // require methodOverride
-const methodOverride = require('method-override');
+const methodOverride = require('method-override')
 // require router
-const routes = require('./routes');
+const routes = require('./routes')
 // require passport
 const usePassport = require('./config/passport')
 //require connect flash
 const flash = require('connect-flash')
 
 // require mongoose config
-require('./config/mongoose');
+require('./config/mongoose')
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-const port = process.env.PORT;
+const port = process.env.PORT
 // setting template engine
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 // setting static files
-app.use(express.static('public'));
+app.use(express.static('public'))
 // 每筆請求都要透過 body-parser 作前置處理
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 // 每筆請求都會先以 methodOverride 進行前置處理
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method'))
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
@@ -52,5 +52,5 @@ app.use(routes);
 
 // start and listen on the Express server
 app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`);
+  console.log(`Express is listening on localhost:${port}`)
 });
--- routes/modules/search.js
+++ routes/modules/search.js
@@ -1,38 +1,39 @@
 // require Express and Express router
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // require restaurant model
-const restaurant = require('../../models/restaurant');
+const restaurant = require('../../models/restaurant')
 
 // search
 router.get('/', (req, res) => {
   if (!req.query.keyword) {
-    return res.redirect('/');
+    return res.redirect('/')
   }
 
-  const keyword = req.query.keyword.trim().toLowerCase();
-  const sort = req.query.sort;
-  let sortKeyword = { _id: 'asc' };
+  const userId = req.user._id
+  const keyword = req.query.keyword.trim().toLowerCase()
+  const sort = req.query.sort
+  let sortKeyword = { _id: 'asc' }
 
   if (sort === 'A > Z') {
-    sortKeyword = { name: 'asc' };
+    sortKeyword = { name: 'asc' }
   } else if (sort === 'Z > A') {
-    sortKeyword = { name: 'desc' };
+    sortKeyword = { name: 'desc' }
   } else if (sort === '類別') {
-    sortKeyword = { category: 'asc' };
+    sortKeyword = { category: 'asc' }
   } else if (sort === '地區') {
-    sortKeyword = { location: 'asc' };
+    sortKeyword = { location: 'asc' }
   }
 
   let sortWord = sort;
 
   if (!sort.includes(' (目前的排序)')) {
-    sortWord = sort + ' (目前的排序)';
+    sortWord = sort + ' (目前的排序)'
   }
 
   restaurant
-    .find()
+    .find({userId})
     .lean()
     .sort(sortKeyword)
     .then((restaurantsData) => {
@@ -48,7 +49,7 @@ router.get('/', (req, res) => {
         sort: sortWord,
       });
     })
-    .catch((error) => console.log(error));
+    .catch((error) => console.log(error))
 });
 
 // export router
